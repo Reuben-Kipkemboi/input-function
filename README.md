023e86c8ec4b9c4f0a72a5fe48d02929e13bf2b7
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2024-02-26 22:40:33 +0300

    dc(Readme) - updates readme file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# input-function
+### input-function program
+A simple python program that:
+  - Takes user input of Name, Age and location
+   - Prints a line to output the above
+
+### Running the program
+1. Have python installed.
+2. Clone the repo.
+3. open terminal and run `python user_input.py`
--- user_input.py
+++ user_input.py
@@ -2,6 +2,5 @@ name = input('Enter your Name: ')
 age = input('Enter your age: ')
 location = input('Enter your Location: ')
 
-print('Hello ' + name + ',')
-print('Your age is: ' + age)
-print('You Reside in: ' + location)
+# output it as one line we have the variables as strings so as to concatenate
+print('Hello ' + name + ', Your age is: ' + str(age) + ', You Reside in: ' + location)
