be00e78663e2b93bc1c9381db0327c1c7ad493af
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2024-02-26 22:28:31 +0300

    ft(function) - simple input method to get user response

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input('Enter your Name: ')
+age = input('Enter your age: ')
+location = input('Enter your Location: ')
+
+print('Hello ' + name + ',')
+print('Your age is: ' + age)
+print('You Reside in: ' + location)
