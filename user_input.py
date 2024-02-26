f2ed38f2d67c6fc8f4792174ba068f939a09d922
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2024-02-26 22:47:32 +0300

    ft(function) - update print statement

--- user_input.py
+++ user_input.py
@@ -3,4 +3,4 @@ age = input('Enter your age: ')
 location = input('Enter your Location: ')
 
 # output it as one line we have the variables as strings so as to concatenate
-print('Hello ' + name + ', Your age is: ' + str(age) + ', You Reside in: ' + location)
+print('Hello ' + name + ', Your age is ' + str(age) + ', You Reside in ' + location)
